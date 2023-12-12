6a622fe0050be06176780ed9ba305ff17a4600f3
Julien Varachas <varachasjulien@gmail.com>
2023-12-12 17:17:29 +0100

    Maj responsive

--- main.php
+++ main.php
@@ -14,7 +14,7 @@ require_once ('index.php');
       </div>
       <div class="image-text pb-2">
         <h1 class="">SEMI-MARATHON <br>DE<br> LA ROCHELLE<br>AVEC<br>HYUNDAI LA ROCHELLE</h1>
-        <img src="assets/partenaires/logo_Hyundai.png">
+        <img src="assets/partenaires/logo_Hyundai.png" class="img-fluid" alt="logo Hyundai">
       </div>
       <div class="image-club">
         <a href="http://aunisathletismelarochelleaytre.athle.com/"><img src="assets/icones/logo Aunis vectorise.jpg" class="img-fluid" alt="Aunis Athlétisme"></a>
