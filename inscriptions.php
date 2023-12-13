2296d75b5433bf0b9236d886e04ba97e6f6e1f48
Julien Varachas <varachasjulien@gmail.com>
2023-12-13 11:49:10 +0100

    ajustement style

--- inscriptions.php
+++ inscriptions.php
@@ -11,8 +11,8 @@ require_once ('index.php')
     </div>
 </div>
 
-<div class="d-flex flex-column justify-content-center text-center mt-5">
-    <a href=" http://www.klikego.com/inscription/semi-marathon-de-la-rochelle-2024/course-a-pied-running/1419371741424-28"><img src="assets/images/logoKlikegoV2.png" class=" klikego img-fluid"></a>
+<div class="d-flex flex-column justify-content-center text-center mb-3 mt-5">
+    <a href=" http://www.klikego.com/inscription/semi-marathon-de-la-rochelle-2024/course-a-pied-running/1419371741424-28"><img src="assets/images/logoKlikegoV2.png" class=" klikego img-fluid mb-2"></a>
     <p class="italic">Cliquez sur le logo pour accéder à la plateforme d'inscription</p>
 
 </div>
