1a61bc88671fbaf651de6020317bf3b261c03b10
Julien Varachas <varachasjulien@gmail.com>
2023-12-15 11:54:09 +0100

    modif mentions legales

--- mentions_legales.php
+++ mentions_legales.php
@@ -10,12 +10,12 @@ require_once ('index.php');
         <div class="justified-text mt-3">
             <ol>
                 <li><h3>Editeur du site</h3></li>
-                <p>Le site semimarathonlarochelle.fr (ci-après dénommé "le Site") est édité par Monsieur Varachas, 
+                <p>Le site semimarathonlarochelle.fr (ci-après dénommé "le Site") est édité par Monsieur Varachas J., 
                     un bénévole indépendant, agissant à titre personnel et n'appartenant à aucune entité légale.
                 </p>
                                 
                 <li><h3>Hébegement</h3></li>
-                <p>Le Site est hébergé par alwaysdata.net, dont le siège social est situé à - 91 rue du Faubourg Saint Honoré - 75008 Paris -.</p>
+                <p>Le Site est hébergé par Alwaysdata.net, dont le siège social est situé à - 91 rue du Faubourg Saint Honoré - 75008 Paris -.</p>
 
                 <li><h3>Propriété intellectuelle</h3></li>
                 <p>Le Site et chacun des éléments qui le composent (tels que marques, images, textes, vidéos, etc.) 
@@ -24,23 +24,23 @@ require_once ('index.php');
                 <p>La reproduction et l'utilisation de tout ou partie de ces éléments est seulement autorisée aux fins exclusives</p>
 
                 <li><h3>Données personnelles</h3></li>
-                <p>Les données personnelles collectées sur le Site sont exclusivement destinées à [Nom de l'entreprise] 
+                <p>Les données personnelles collectées sur le Site sont exclusivement destinées à Aunis Athlétisme La Rochelle Aytré, 
                     et ne seront en aucun cas communiquées à des tiers.
                 </p>
 
                 <li><h3>Cookies</h3></li>
                 <p>Le Site utilise des cookies. Ces fichiers stockés sur votre ordinateur nous servent à faciliter votre accès aux services que nous proposons. 
                     Les cookies du site ne contiennent pas de données permettant de vous identifier personnellement, 
-                    et ils sont conçus pour être utilisés uniquement par Monsieur Varachas. 
+                    et ils sont conçus pour être utilisés uniquement par Monsieur Varachas J.. 
                     Nous vous informons que vous pouvez vous opposer à l'enregistrement de ces "cookies" 
                     en configurant votre ordinateur selon les modalités détaillées sur le site http://www.cnil.fr.
                 </p>
 
                 <li><h3>Responsabilité</h3></li>
-                <p>Monsieur Varachas s'efforce d'assurer au mieux de ses possibilités, 
+                <p>Monsieur Varachas J. s'efforce d'assurer au mieux de ses possibilités, 
                     l'exactitude et la mise à jour des informations diffusées sur le Site, 
                     dont elle se réserve le droit de corriger, à tout moment et sans préavis, le contenu. 
-                    Toutefois, Monsieur Varachas ne peut garantir l'exactitude, 
+                    Toutefois, Monsieur Varachas J. ne peut garantir l'exactitude, 
                     la précision ou l'exhaustivité des informations mises à la disposition sur le Site.
                 </p>
 
