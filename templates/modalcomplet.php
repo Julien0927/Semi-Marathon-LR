eae63eda395da1aea1f27cf59cbc50355901871f
Julien Varachas <varachasjulien@gmail.com>
2024-01-23 13:56:48 +0100

    MAJ Modale

--- templates/modalcomplet.php
+++ templates/modalcomplet.php
@@ -3,7 +3,7 @@ require_once ('templates/header.php');
 ?>
 
 <!-- Modal -->
-<div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!--<div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header mx-auto">
@@ -15,6 +15,25 @@ require_once ('templates/header.php');
       Une communication sera établie quelques jours avant.<br>
       Surveillez nos réseaux sociaux !
     </div>
+    <div class="text-center mb-3">
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fermer</button>
+    </div>
+    </div>
+  </div>
+</div>-->
+<div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header mx-auto">
+        <h1 class="modal-title text-focus-in" id="exampleModalLabel">FLASH INFO!</h1>
+      </div>
+      <div class="modal-body">
+      Ouverture d'une vente spéciale le jeudi 1er février à midi<br>
+      <a href="https://www.klikego.com/inscription/semi-marathon-de-la-rochelle-2024/course-a-pied-running/1419371741424-28?fbclid=IwAR0om6fgGx01QEaeWWto2Bh86tz_n-jdRrPHuy0cqQY4mKDNvh20Ly_6f9o">Uniquement sur klikego</a><br>
+      200 dossards pour le semi-marathon<br>
+      20 équipes pour le duo<br>
+      Bonne dernière chance à tous
+      </div>
     <div class="text-center mb-3">
         <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fermer</button>
     </div>
