d9038bf83169869ae418d056a781f5c4a335c5fd
Julien Varachas <varachasjulien@gmail.com>
2024-11-02 14:40:22 +0100

    MAJ Item animation

--- templates/items.php
+++ templates/items.php
@@ -1,30 +1,30 @@
 <div class="d-flex flex-md-row flex-column justify-content-around text-center mt-0">
     <div>
-      <a href="courses.php"><img src="assets/icones/icon course.svg" alt="parcours des courses"></a>
+      <a href="courses.php"><img src="assets/icones/icon course.svg" class="itemChoice" alt="parcours des courses"></a>
       <h6>COURSES</p></h6>
       <p>Découvrez les parcours</p>
     </div>
 
     <div>
-      <a href="inscriptions.php"><img src="assets/icones/icon inscription.svg" alt="inscriptions courses"></a>
+      <a href="inscriptions.php"><img src="assets/icones/icon inscription.svg" class="itemChoice" alt="inscriptions courses"></a>
       <h6>INSCRIPTIONS</h6>
       <p>Les inscriptions pour <br>l'édition 2025</p>
     </div>
 
     <div>
-      <a href="infos.php"><img src="assets/icones/icon info.svg" alt="Retrouvez les infos pratiques"></a>
+      <a href="infos.php"><img src="assets/icones/icon info.svg" class="itemChoice" alt="Retrouvez les infos pratiques"></a>
       <h6>INFOS PRATIQUES</h6>
       <p>Toutes les réponses sont <br> là</p>
     </div>
 
     <div>
-      <a href="medias.php"><img src="assets/icones/icon media.svg" alt="photos des éditions précédentes"></a>
+      <a href="medias.php"><img src="assets/icones/icon media.svg" class="itemChoice" alt="photos des éditions précédentes"></a>
       <h6>MEDIAS</h6>
       <p>Retrouvez toutes les photos</p>
     </div>
 
     <div>
-      <a href="resultats.php"><img src="assets/icones/icon medal.svg" alt=""></a>
+      <a href="resultats.php"><img src="assets/icones/icon medal.svg" class="itemChoice" alt=""></a>
         <h6>RESULTATS</h6>
         <p>Retrouvez les résultats </p>
     </div>
