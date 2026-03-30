b134739d7435393b692be5f9c14dceab37665236
Julien Varachas <varachasjulien@gmail.com>
2026-03-30 19:55:33 +0200

    MAJ results

--- resultats.php
+++ resultats.php
@@ -11,28 +11,44 @@ require_once ('templates/header.php');
     </div>
 </div>
 <div class="row mt-5">
-    <div class="d-flex"></div>
-        <div class="col-md-6 justify-content-center text-center">
+    <div class="d-flex">
+        <div class="col-12 col-md-3 justify-content-center text-center" >
             <h3>Classement 2023</h3>
             <a href="https://bases.athle.fr/asp.net/liste.aspx?frmbase=resultats&frmmode=1&frmespace=0&frmcompetition=266488"><img src="assets/icones/podium.png" alt="résultats"></a>
             <p class="italic">Cliquez sur l'icône pour découvrir le classement</p>
-            <h3 class="mt-3">Classement 2024</h3>
+        </div>
+        <div class="col-12 col-md-3 justify-content-center text-center" >
+            <h3>Classement 2024</h3>
             <a href="http://bases.athle.fr/asp.net/liste.aspx?frmbase=resultats&amp;frmmode=1&amp;frmespace=0&amp;frmcompetition=280919&amp;frmepreuve=5+Km+Route+TCX"><img src="assets/icones/podium.png" alt="résultats"></a>
             <p class="italic">Cliquez sur l'icône pour découvrir le classement</p>
-            <h3 class="mt-3">Classement 2025</h3>
+        </div>
+        <div class="col-12 col-md-3 justify-content-center text-center" >
+            <h3>Classement 2025</h3>
             <a href="https://bases.athle.fr/asp.net/liste.aspx?frmbase=resultats&frmmode=1&frmespace=0&frmcompetition=294435"><img src="assets/icones/podium.png" alt="résultats"></a>
             <p class="italic">Cliquez sur l'icône pour découvrir le classement</p>
         </div>
+        <div class="col-12 col-md-3 justify-content-center text-center" >
+            <h3>Classement 2026</h3>
+            <a href="https://resultats.breizhchrono.com/resultats-courses/semi-marathon-de-la-rochelle-2026-1488071608761-832/5-km"><img src="assets/icones/podium.png" alt="résultats"></a>
+            <p class="italic">Cliquez sur l'icône pour découvrir le classement</p>
+        </div>
+    </div>
+</div>
+<div class="row mt-5">
+    <div class="d-flex">
         <div class="col-md-6 justify-content-center d-flex flex-column text-center">
             <h3>Palmarès masculin</h3>
             <a href="http://aunisathletismelarochelleaytre.athle.com/asp.net/espaces.html/html.aspx?id=40029"><img src="assets/icones/badge.png" alt="palmares"></a>
             <p class="italic">Cliquez sur l'icône pour découvrir le palmarès</p>
-            <h3 class="mt-3">Palmarès féminin</h3>
+        </div>
+        <div class="col-md-6 justify-content-center d-flex flex-column text-center">
+            <h3>Palmarès féminin</h3>
             <a href="http://aunisathletismelarochelleaytre.athle.com/asp.net/espaces.html/html.aspx?id=42603"><img src="assets/icones/badge.png" alt="palmares"></a>
             <p class="italic">Cliquez sur l'icône pour découvrir le palmarès</p>
         </div>
     </div>
 </div>
+
 
 <?php 
 require_once ('templates/footer.php')
